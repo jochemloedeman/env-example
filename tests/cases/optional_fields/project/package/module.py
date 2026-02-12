8df354a595892a5ec756367397e75116dbb6486e
Jochem Loedeman <60843521+jochemloedeman@users.noreply.github.com>
2026-02-12 13:46:44 +0100

    add ignore settings subcases

--- tests/cases/optional_fields/project/package/module.py
+++ tests/cases/optional_fields/project/package/module.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     required_field: int
-    still_required: int = Field(..., frozen=True)
-    optional_field: str | None = None
-    another_optional_field: str | None = Field(default=None)
+    explicit_required: int = Field(..., frozen=True)
+    implicit_required: int = Field(frozen=True)
+    plain_default: str | None = None
+    keyword_default: str | None = Field(default=None)
+    positional_default: int = Field(42)
+    factory_default: list[str] = Field(default_factory=list)
